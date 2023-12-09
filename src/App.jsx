1c9356d1416d9c88672a9d7b233594081b298207
IN-Accurate <vishalsankar410@gmail.com>
2023-12-09 19:25:52 +0530

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,24 @@
-import "./App.css";
-// import {useEffect, useState} from 'react';
-import Navbar from "./Navbar/Navbar";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Contact from "./Contact/Contact";
-import Events from "./Events/Events";
-import Home from "./Home/Home";
-import IndividualEvents from "./Events/IndividualEvents";
-// import Loader from './Threescene/Loader/Loader';
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home />,
-		errorElement: <div>Something went wrong</div>,
-	},
-	{
-		path: "/contact-us",
-		element: <Contact />,
-		errorElement: <div>Something went wrong</div>,
-	},
-	{
-		path: "/events",
-		element: <Events />,
-		errorElement: <div>Something went wrong</div>,
-	},
-	{
-		path: "/events/:route",
-		element: <IndividualEvents />,
-		errorElement: <div>Something went wrong</div>,
-	},
-]);
+import './App.css';
+import Contact from './Contact/Contact';
+import Events from './Events/Events';
+import Home from './Home/Home';
+import IndividualEvents from './Events/IndividualEvents';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-	// const [loading, setLoading] = useState(true);
-
-	// useEffect(() => {
-	// 	const loadingTimer = setTimeout(() => {
-	// 		setLoading(false);
-	// 	}, 1500);
-	// 	return () => clearTimeout(loadingTimer);
-	// }, []);
-
-	return (
-		<>
-			{/* {loading ? (
-				<div className="flex h-screen items-center justify-center bg-black">
-					<Loader />
-				</div>
-			) : ( */}
-			<>
-				<RouterProvider router={router} />
-			</>
-			{/* )} */}
-		</>
-	);
+  return (
+    <>
+      {' '}
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact-us' element={<Contact />} />
+          <Route path='/events' element={<Events />} />
+          <Route path='/events/:route' element={<IndividualEvents />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
