180f99b0f98c68006b34ae35363118d118616282
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-12-09 15:05:56 +0530

    Postprocessing

--- src/Threescene/Threescene.jsx
+++ src/Threescene/Threescene.jsx
@@ -4,7 +4,8 @@ import { ScrollControls } from "@react-three/drei";
 import { useEffect, useState } from "react";
 import  Loader  from "./Loader/Loader"
 import { useProgress } from "@react-three/drei";
-
+import { EffectComposer, Vignette,BrightnessContrast, Noise, HueSaturation } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
 function Threescene() {
 
   const [loading, setLoading] = useState(false)
@@ -25,10 +26,23 @@ function Threescene() {
   return (
     <>
       {loading?(<Loader/>):(<></>)}
-      <Canvas linear={true}>
+      <Canvas>
           <ScrollControls pages={25} damping={0.2}>
             <Experience />
           </ScrollControls>
+          <EffectComposer>
+            <Vignette eskil={false} offset={0.3} darkness={0.55}/>
+            <Noise opacity={0.02}/>
+            <BrightnessContrast
+              brightness={0} // brightness. min: -1, max: 1
+              contrast={0.05} // contrast: min -1, max: 1
+            />
+            <HueSaturation
+              blendFunction={BlendFunction.NORMAL} // blend mode
+              hue={-0.05} // hue in radians
+              saturation={0.1} // saturation in radians
+            />
+        </EffectComposer>
       </Canvas>
     </>
   );
