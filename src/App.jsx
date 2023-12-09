181d5a6d052116f9883bbc3ddd7f45d91c0477c5
varun3003 <varunpradeep30@gmail.com>
2023-12-10 00:18:26 +0530

    interactive bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Contact from "./Contact/Contact";
 import Events from "./Events/Events";
 import Home from "./Home/Home";
 import IndividualEvents from "./Events/IndividualEvents";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+	useEffect(() => {
+		const ratio = 0.05;
+		let x;
+		let y;
+
+		const handleMouseMove = (e) => {
+			x = e.pageX;
+			y = e.pageY;
+		};
+
+		const animation = () => {
+			const z1 = document.getElementsByClassName("z-1")[0];
+			const z2 = document.getElementsByClassName("z-2")[0];
+			const z3 = document.getElementsByClassName("z-3")[0];
+
+			if (z1 && z2 && z3) {
+				z1.style.transform = `translate(${x * ratio}px, ${y * ratio}px)`;
+				z2.style.transform = `translate(${(x * ratio) / 2}px, ${
+					(y * ratio) / 2
+				}px) rotate(217deg)`;
+				z3.style.transform = `translate(${(x * ratio) / 3}px, ${
+					(y * ratio) / 3
+				}px) rotate(71deg)`;
+			}
+
+			requestAnimationFrame(animation);
+		};
+
+		document.addEventListener("mousemove", handleMouseMove);
+		animation();
+
+		return () => {
+			document.removeEventListener("mousemove", handleMouseMove);
+		};
+	}, []); // empty dependency array means this effect will only run once on mount
+
 	return (
 		<>
+			{/* Your existing JSX code */}
 			<div class="bg">
 				<div class="z-3">
 					<div class="tile top-left animate-opacity freq-5"></div>
