8bf71fa0a6f395320cad181be0fdabbec6329e3f
varun3003 <varunpradeep30@gmail.com>
2023-12-09 16:31:42 +0530

    events page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,13 @@
-import Threescene from '../Threescene/Threescene';
-import './Home.css';
+import Threescene from "../Threescene/Threescene";
+import "./Home.css";
 function Home() {
-  return (
-    <div className='home-container'>
-      <Threescene />
-    </div>
-  );
+	return (
+		<div className="home-container">
+			<div className="canvas-container">
+				<Threescene />
+			</div>
+		</div>
+	);
 }
 
 export default Home;
